75a66453412797735cea8935c93d61aaae8416ac
atnbn <altan.bnm@gmail.com>
2024-01-30 14:14:38 +0100

    add

--- src/app/core/services/base-api/base-api.service.ts
+++ src/app/core/services/base-api/base-api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
-import { environment } from 'src/app/environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
