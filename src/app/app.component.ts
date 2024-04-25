b53c7cfcef3ea60d715a312387e7a29b6e2899bb
atnbn <altan.bnm@gmail.com>
2024-04-25 17:44:40 +0200

    fix visual bugs , fix notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { OpenPopUpService } from './shared/services/add-board/add-board-up.service';
-import { ThemeService } from './shared/services/theme/theme.service';
-import { SidebarService } from './shared/services/sidebar/sidebar.service';
-import { trigger, transition, style, animate } from '@angular/animations';
-import { Board } from './shared/models/board-interface';
-import { BoardObjectService } from './shared/services/add-board/board-object.service';
-import { CreateUserService } from './shared/services/user/create-user/create-user.service';
-import { environment } from 'src/environments/environment';
-import { ServerStatusService } from './shared/services/server-status/server-status.service';
 import { AuthUserService } from './shared/services/user/login-user/login-user.service';
-import { Route, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from './shared/services/language/language.service';
 @Component({
